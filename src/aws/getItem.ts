2c3388bbf1eae4c44ae2c90e23572bee1305de02
kcabo <37576764+kcabo@users.noreply.github.com>
2021-11-11 12:13:35 +0000

    ✨ add getUser logic

---
+++ src/aws/getItem.ts
@@ -0,0 +1,48 @@
+import { isDbUserItem } from '../typeChecker';
+import { documentClient, RACE_TABLE_NAME } from './dynamodb';
+
+type dbGetResult =
+  | {
+      [key: string]: any;
+    }
+  | {
+      error: true;
+    }
+  | undefined;
+
+async function getRequest(userId: string, sk: string): Promise<dbGetResult> {
+  try {
+    const { Item } = await documentClient.get({
+      TableName: RACE_TABLE_NAME,
+      Key: {
+        userId: userId,
+        sk: sk,
+      },
+    });
+
+    return Item;
+  } catch (error) {
+    console.error(
+      `Get Request Failed on userId=${userId} and sk=${sk}:`,
+      JSON.stringify(error, null, 2)
+    );
+    return { error: true };
+  }
+}
+
+export async function getUser(userId: string) {
+  const sk = 'USER#' + userId;
+  const item = await getRequest(userId, sk);
+
+  if (!item) {
+    console.error('Cannot find user:', userId);
+    return undefined;
+  } else if (item.error === true) {
+    return undefined;
+  } else if (!isDbUserItem(item)) {
+    console.error('Invalid User item:', JSON.stringify(item, null, 2));
+    return undefined;
+  }
+
+  return item;
+}
--- src/aws/index.ts
+++ src/aws/index.ts
@@ -1,3 +1,4 @@
 export { generateURLforDownload } from './s3Presign';
 export { requestGenerateSheet } from './callLambda';
 export { putNewRace } from './putItem';
+export { getUser } from './getItem';
