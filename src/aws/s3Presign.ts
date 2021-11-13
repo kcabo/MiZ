652bed9481bf99c30225be15878a3a77c2f663c2
kcabo <37576764+kcabo@users.noreply.github.com>
2021-11-13 04:00:00 +0000

    ♻️ change variable name: OBJECT_DOWNLOAD_LIMIT_SECONDS

--- src/aws/s3Presign.ts
+++ src/aws/s3Presign.ts
@@ -2,7 +2,8 @@ import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 const BUCKET_NAME = process.env.PAPARAZZO_BUCKET_NAME;
-const OBJECT_DOWNLOAD_LIMIT = Number(process.env.OBJECT_DOWNLOAD_LIMIT) || 100;
+const OBJECT_DOWNLOAD_LIMIT_SECONDS =
+  Number(process.env.OBJECT_DOWNLOAD_LIMIT_SECONDS) || 100;
 const s3 = new S3Client({ region: 'ap-northeast-1' });
 
 export async function generateURLforDownload(objectKey: string) {
@@ -12,7 +13,7 @@ export async function generateURLforDownload(objectKey: string) {
   };
   const command = new GetObjectCommand(param);
   const url = await getSignedUrl(s3, command, {
-    expiresIn: OBJECT_DOWNLOAD_LIMIT,
+    expiresIn: OBJECT_DOWNLOAD_LIMIT_SECONDS,
   });
   return url;
 }
