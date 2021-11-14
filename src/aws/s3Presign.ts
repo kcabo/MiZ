7035811fb0b576f9e0a6fb7a774160f6d384d763
kcabo <37576764+kcabo@users.noreply.github.com>
2021-11-14 09:24:20 +0000

    🍰 changed enviroment variable name

--- src/aws/s3Presign.ts
+++ src/aws/s3Presign.ts
@@ -2,8 +2,7 @@ import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 const BUCKET_NAME = process.env.PAPARAZZO_BUCKET_NAME;
-const OBJECT_DOWNLOAD_LIMIT_SECONDS =
-  Number(process.env.OBJECT_DOWNLOAD_LIMIT_SECONDS) || 100;
+const PRESIGN_EXPIRES_IN = Number(process.env.PRESIGN_EXPIRES_IN) || 100;
 const s3 = new S3Client({ region: 'ap-northeast-1' });
 
 export async function generateURLforDownload(objectKey: string) {
@@ -13,7 +12,7 @@ export async function generateURLforDownload(objectKey: string) {
   };
   const command = new GetObjectCommand(param);
   const url = await getSignedUrl(s3, command, {
-    expiresIn: OBJECT_DOWNLOAD_LIMIT_SECONDS,
+    expiresIn: PRESIGN_EXPIRES_IN,
   });
   return url;
 }
--- src/environment.d.ts
+++ src/environment.d.ts
@@ -6,6 +6,7 @@ declare global {
       PAPARAZZO_FUNCTION_ARN: string;
       PAPARAZZO_BUCKET_NAME: string;
       DYNAMODB_RACE_TABLE_NAME: string;
+      PRESIGN_EXPIRES_IN: string;
     }
   }
 }
