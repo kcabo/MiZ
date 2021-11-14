db6f88036f0ba6ecd2305b381e1e34f134a1d13d
kcabo <37576764+kcabo@users.noreply.github.com>
2021-11-14 10:51:37 +0000

    🐛 early return on blockedByUser

--- src/blockedByUser.ts
+++ src/blockedByUser.ts
@@ -1,11 +1,12 @@
 import ErrorLog from 'logger';
 import { getUser, updateUser } from 'aws';
 
-export async function blockedByUser(userId: string) {
+export async function blockedByUser(userId: string): Promise<void> {
   // ユーザーの存在を一応確認
   const user = await getUser(userId);
   if (!user) {
     ErrorLog('Cannot find user:', userId);
+    return;
   }
 
   // ユーザー情報の書き換え
