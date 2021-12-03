5bb58865416d779c72f3d13f117dc3de64a16576
kcabo <37576764+kcabo@users.noreply.github.com>
2021-12-03 10:45:59 +0000

    💄 refine confirm delete message

--- src/lineApi/replies/confirmDeleteRace.ts
+++ src/lineApi/replies/confirmDeleteRace.ts
@@ -15,71 +15,159 @@ export function confirmDeleteRace(raceId: string, race: Race): FlexMessage {
     altText: 'レースデータを削除しますか？',
     contents: {
       type: 'bubble',
+      header: {
+        type: 'box',
+        layout: 'baseline',
+        contents: [
+          {
+            type: 'icon',
+            url: 'https://miz-assets.s3.ap-northeast-1.amazonaws.com/icons/warning.png',
+            size: '28px',
+            offsetTop: '8px',
+          },
+          {
+            type: 'text',
+            text: '以下のレースを削除しますか？',
+            color: '#A16207',
+            weight: 'bold',
+          },
+        ],
+        backgroundColor: '#FEFCE8',
+        spacing: 'md',
+        paddingTop: '12px',
+      },
       body: {
         type: 'box',
         layout: 'vertical',
         contents: [
           {
-            type: 'text',
-            text: '本当に以下のデータを削除しますか？削除しない場合は無視してください',
-            size: 'sm',
+            type: 'box',
+            layout: 'baseline',
+            contents: [
+              {
+                type: 'text',
+                text: '大会名',
+                size: '12px',
+                flex: 0,
+              },
+              {
+                type: 'text',
+                text: race.meet || '-',
+                flex: 1,
+                size: '16px',
+                weight: 'bold',
+                color: '#3F3F46',
+              },
+            ],
+            spacing: 'lg',
           },
+          {
+            type: 'box',
+            layout: 'baseline',
+            contents: [
+              {
+                type: 'text',
+                text: '日付　',
+                size: '12px',
+                flex: 0,
+              },
+              {
+                type: 'text',
+                text: race.date,
+                flex: 1,
+                size: '16px',
+                weight: 'bold',
+                color: '#3F3F46',
+              },
+            ],
+            spacing: 'lg',
+          },
+          {
+            type: 'box',
+            layout: 'baseline',
+            contents: [
+              {
+                type: 'text',
+                text: '選手名',
+                size: '12px',
+                flex: 0,
+              },
+              {
+                type: 'text',
+                text: race.swimmer,
+                flex: 1,
+                size: '16px',
+                weight: 'bold',
+                color: '#3F3F46',
+              },
+            ],
+            spacing: 'lg',
+          },
+          {
+            type: 'box',
+            layout: 'baseline',
+            contents: [
+              {
+                type: 'text',
+                text: '種目名',
+                size: '12px',
+                flex: 0,
+              },
+              {
+                type: 'text',
+                text: race.event,
+                flex: 1,
+                size: '16px',
+                weight: 'bold',
+                color: '#3F3F46',
+              },
+            ],
+            spacing: 'lg',
+          },
+        ],
+        spacing: 'lg',
+        paddingAll: 'xxl',
+      },
+      footer: {
+        type: 'box',
+        layout: 'vertical',
+        contents: [
           {
             type: 'box',
             layout: 'vertical',
             contents: [
               {
                 type: 'text',
-                text: '大会名' + (race.place || '-'),
-              },
-              {
-                type: 'text',
-                text: '日付' + race.date,
-              },
-              {
-                type: 'text',
-                text: '選手名' + race.swimmer,
-              },
-              {
-                type: 'text',
-                text: '種目' + race.event,
+                text: '削除する',
+                color: '#EF4444',
+                weight: 'bold',
+                align: 'center',
               },
             ],
-          },
-          {
-            type: 'box',
-            layout: 'horizontal',
-            contents: [
-              {
-                type: 'text',
-                text: '削除',
-                action: {
-                  type: 'postback',
-                  label: '-',
-                  data: JSON.stringify(deleteRaceActionPayload),
-                  displayText: '[削除]',
-                },
-              },
-            ],
-            paddingAll: 'md',
-          },
-          {
-            type: 'text',
-            text: '画像が表示されない・表示が崩れている場合は再生成を試してください',
-            wrap: true,
-          },
-          {
-            type: 'text',
-            text: '画像を再生成',
-            color: '#559ab9',
+            cornerRadius: 'md',
+            paddingAll: 'lg',
+            borderWidth: '2px',
+            borderColor: '#EF4444',
             action: {
               type: 'postback',
-              label: 'action',
-              data: 'hello',
-              displayText: '[画像を再生成]',
+              label: '-',
+              data: JSON.stringify(deleteRaceActionPayload),
+              displayText: '[削除]',
             },
           },
+          {
+            type: 'text',
+            text: '削除しない場合はこのまま無視してください',
+            color: '#3F3F46',
+            size: 'xxs',
+            offsetTop: '14px',
+            align: 'center',
+          },
         ],
+        paddingTop: '0px',
+        paddingStart: 'xxl',
+        paddingEnd: 'xxl',
+        paddingBottom: '30px',
       },
     },
   };
