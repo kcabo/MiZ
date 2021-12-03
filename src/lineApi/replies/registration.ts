23d965c2ebb5c09a646a6583440503541d7f9c7e
kcabo <37576764+kcabo@users.noreply.github.com>
2021-12-03 11:16:01 +0000

    💄 refine registration message

--- src/lineApi/replies/registration.ts
+++ src/lineApi/replies/registration.ts
@@ -15,55 +15,82 @@ export function registration(): FlexMessage {
     altText: 'はじめまして！MiZです',
     contents: {
       type: 'bubble',
-      body: {
+      header: {
         type: 'box',
-        layout: 'horizontal',
+        layout: 'vertical',
         contents: [
           {
-            type: 'box',
-            layout: 'vertical',
-            contents: [
-              {
-                type: 'image',
-                url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
-                size: 'lg',
-                aspectMode: 'cover',
-                action: {
-                  type: 'postback',
-                  label: '-',
-                  data: JSON.stringify(startAsSwimmerPostback),
-                  displayText: '[選手として始める]',
-                },
-              },
-              {
-                type: 'text',
-                text: '選手',
-              },
-            ],
+            type: 'text',
+            text: 'モードを選ぶ',
+            weight: 'bold',
+            color: '#FFFFFF',
+            size: '24px',
           },
           {
-            type: 'box',
-            layout: 'vertical',
-            contents: [
-              {
-                type: 'image',
-                url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
-                size: 'lg',
-                aspectMode: 'cover',
-                action: {
-                  type: 'postback',
-                  label: '-',
-                  data: JSON.stringify(startAsManagerPostback),
-                  displayText: '[マネージャーとして始める]',
-                },
-              },
-              {
-                type: 'text',
-                text: 'マネージャー',
-              },
-            ],
+            type: 'text',
+            text: 'どちらかを選択してください\n ※ モードはいつでも変更できます',
+            wrap: true,
+            weight: 'bold',
+            color: '#FFFFFF',
+            size: 'xs',
           },
         ],
+        backgroundColor: '#3399FF',
+        spacing: 'md',
+        paddingAll: 'xxl',
+        paddingTop: '40px',
+      },
+      body: {
+        type: 'box',
+        layout: 'vertical',
+        contents: [
+          {
+            type: 'image',
+            url: 'https://miz-assets.s3.ap-northeast-1.amazonaws.com/start_as_swimmer.png',
+            size: 'full',
+            aspectRatio: '400:104',
+            action: {
+              type: 'postback',
+              label: '-',
+              data: JSON.stringify(startAsSwimmerPostback),
+              displayText: '[選手として始める]',
+            },
+          },
+          {
+            type: 'image',
+            url: 'https://miz-assets.s3.ap-northeast-1.amazonaws.com/start_as_manager.png',
+            size: 'full',
+            aspectRatio: '400:104',
+            action: {
+              type: 'postback',
+              label: '-',
+              data: JSON.stringify(startAsManagerPostback),
+              displayText: '[マネージャーとして始める]',
+            },
+          },
+        ],
+        spacing: 'lg',
+        paddingAll: 'xl',
+        paddingBottom: '0px',
+      },
+      footer: {
+        type: 'box',
+        layout: 'vertical',
+        contents: [
+          {
+            type: 'text',
+            text: 'サービスを利用することで、利用規約とプライバシーポリシーに同意したとみなします。',
+            color: '#3F3F46',
+            size: 'xs',
+            offsetTop: '14px',
+            wrap: true,
+            weight: 'bold',
+          },
+        ],
+        paddingTop: 'none',
+        paddingBottom: '30px',
+        paddingStart: '20px',
+        paddingEnd: '20px',
       },
     },
   };
